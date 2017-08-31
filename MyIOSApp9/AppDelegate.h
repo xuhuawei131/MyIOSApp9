0e16328f9d77db2b34f825e9e2483a37ee832716
lingdian <lingdian@meili-inc.com>
2017-09-01 00:18:13 +0800

    Initial Commit

---
+++ MyIOSApp9/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyIOSApp9
+//
+//  Created by lingdian on 17/9/1.
+//  Copyright © 2017年 lingdian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyIOSApp9/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyIOSApp9
+//
+//  Created by lingdian on 17/9/1.
+//  Copyright © 2017年 lingdian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
